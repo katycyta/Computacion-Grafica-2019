112f91ceeba9aece41cd9219ebdcb78f2b85414f
katycyta <katicita1516@gmail.com>
2019-12-19 18:33:37 -0500

    Update constants.hpp

--- 3raFase/QuadTree/src/constants.hpp
+++ 3raFase/QuadTree/src/constants.hpp
@@ -1,11 +1,3 @@
-/**
- *    > Author:   whpointz
- *    > Mail:     506273980@qq.com
- *    > Github:   https://www.github.com/whpointz
- *    > Description:
- *
- *    > Created Time: 2017/06/24 22:08:45
- **/
 
 #ifndef _CONSTANTS_HPP_
 #define _CONSTANTS_HPP_
